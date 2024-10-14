439d4869493589f7851478e1707b5d7789d33a65
ErwinLambujon <erwinlambujon1@gmail.com>
2024-10-14 23:04:59 +0800

    Added changes

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -3,52 +3,55 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const useAuth = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const Login = async (user) => {
-		try {
-			let response = await axios.post("http://localhost:8000/api/user/login/", {
-				username: user.username,
-				password: user.password,
-			});
+  const Login = async (user) => {
+    try {
+      let response = await axios.post(
+        "https://elevateme-render.onrender.com/api/user/login/",
+        {
+          username: user.username,
+          password: user.password,
+        }
+      );
 
-			if (response.status === 200) {
-				localStorage.setItem("token", response.data.token);
-				navigate("/home", { replace: true });
-			}
-			alert("Login successfully!");
-		} catch (err) {
-			if (err.code === "ERR_BAD_REQUEST") {
-				alert(err.response.data.error);
-			} else {
-				alert("Not connected with the back-end server");
-			}
-		}
-	};
+      if (response.status === 200) {
+        localStorage.setItem("token", response.data.token);
+        navigate("/home", { replace: true });
+      }
+      alert("Login successfully!");
+    } catch (err) {
+      if (err.code === "ERR_BAD_REQUEST") {
+        alert(err.response.data.error);
+      } else {
+        alert("Not connected with the back-end server");
+      }
+    }
+  };
 
-	const Logout = async () => {
-		try {
-			let token = localStorage.getItem("token");
+  const Logout = async () => {
+    try {
+      let token = localStorage.getItem("token");
 
-			await axios.post(
-				"http://localhost:8000/api/user/logout/",
-				{},
-				{
-					headers: { Authorization: `Token ${token}` },
-				}
-			);
+      await axios.post(
+        "https://elevateme-render.onrender.com/api/user/logout/",
+        {},
+        {
+          headers: { Authorization: `Token ${token}` },
+        }
+      );
 
-			localStorage.removeItem("token");
-			sessionStorage.clear();
-			alert("Logout successful!");
+      localStorage.removeItem("token");
+      sessionStorage.clear();
+      alert("Logout successful!");
 
-			navigate("/login", { replace: true });
-		} catch (err) {
-			console.log(err);
-		}
-	};
+      navigate("/login", { replace: true });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	return { Login, Logout };
+  return { Login, Logout };
 };
 
 export default useAuth;
